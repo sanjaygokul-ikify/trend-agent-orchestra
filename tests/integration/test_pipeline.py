93fa9f0834aea70c79e8cc17557e7f662d40d12d
Sanjay G <gokulesh2615@gmail.com>
2026-05-22 14:29:16 +0530

    test(integration): add integration tests for end-to-end pipeline

---
+++ tests/integration/test_pipeline.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from packages.core.engine import Engine
+from packages.core.types import Task
+
+
+class TestPipeline(TestCase):
+    def test_ingest_task(self):
+        engine = Engine()
+        task = Task(id='task-1', priority='LOW', requirements='test-requirement')
+        engine.ingest_task(task)
+        self.assertIn(task.id, engine.tasks)
+        
+    def test_allocate_task(self):
+        engine = Engine()
+        task = Task(id='task-1', priority='LOW', requirements='test-requirement')
+        engine.ingest_task(task)
+        self.assertIsNotNone(engine.find_agent_for_task(task))
+        
+    def test_execute_task(self):
+        engine = Engine()
+        task = Task(id='task-1', priority='LOW', requirements='test-requirement')
+        engine.ingest_task(task)
+        engine.execute_task(task)
+        self.assertEqual(task.status, 'COMPLETED')
